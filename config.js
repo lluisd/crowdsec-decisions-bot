e401a78de5107dca94b4b89cbc9aa5bec3620d64
Lluis Danes Pages <Lluis.Danes@sunwebgroup.com>
2025-04-11 18:04:37 +0200

    remove unused environment variable for telegram chat id

--- config.js
+++ config.js
@@ -2,14 +2,13 @@ export const config = {
     port: process.env.PORT || 3000,
     telegram: {
         token: process.env.TELEGRAM_TOKEN,
-        channelId: process.env.TELEGRAM_CHAT_ID,
         should_use_webhooks: process.env.USE_WEBHOOKS || false,
         externalUrl: process.env.EXTERNAL_URL,
     },
     crowdsec: {
         lapi: {
-            url: process.env.LAPI_URL,
-            login: process.env.LAPI_LOGIN,
+            url: process.env.LAPI_URL || 'http://crowdsec:8080',
+            login: process.env.LAPI_LOGIN || 'crowdsec',
             password: process.env.LAPI_PASSWORD,
         }
     }
